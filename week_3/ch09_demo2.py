5a3750fad14aea1e2f1cc8a18c52eabed4a71b61
Gang Li <ganglix@gmail.com>
2025-11-04 11:30:45 -0600

    in class

--- week_3/ch09_demo2.py
+++ week_3/ch09_demo2.py
@@ -3,3 +3,22 @@
 # while-loop is preferable to using a for-loop.
 
 # password example
+passcode = "cmpt142"
+
+# ask user to start inputing a password
+user_input = input("Enter your password: ")
+count = 1
+n_max_attempt = 3
+
+# keep asking user to type when the user's input is wrong AND the use did not exceed the max trial
+while user_input != passcode and count < n_max_attempt:
+    # keep asking user to type in
+    user_input = input("Enter your password: ")
+    count += 1
+
+# while is done when (user_input != passcode and count <= n_max_attempt) is False
+# there are ONLY two outcomes
+if user_input == passcode:         # either 1 the user got the correct passcode
+    print(f'{count} attempts made')
+else:                              # or 2 the user exceed the max number of attempt
+    print(f'wrong trials reaching maximum {n_max_attempt} attempts')
--- week_3/ch09_ex4.py
+++ week_3/ch09_ex4.py
@@ -7,6 +7,17 @@
 (0,3) (1,3) (2,3) (3,3) (4,3) (5,3) (6,3) (7,3) (8,3) (9,3)
 (0,4) (1,4) (2,4) (3,4) (4,4) (5,4) (6,4) (7,4) (8,4) (9,4)
 """
-# M = 10
-# N = 5
+M = 10
+N = 5
 
+# just do the first line: ( ,0)
+for col in range(M):
+    print('(' + str(col) +',' + '0' + ')', end=' ')  # or use fstring print(f'({col},0)', end=' ')
+print()
+print()
+# do all lines
+for row in range(N):
+    for col in range(M):
+        # for each row repeat the col
+        print('(' + str(col) + ',' + '0' + ')', end=' ')
+    print()  # newline after each row is done
