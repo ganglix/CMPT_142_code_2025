d7a5ef4dd99b555993219fee32da9ff29fdbdf9f
Gang Li <ganglix@gmail.com>
2025-11-18 15:08:18 -0600

    in class

--- week_4/ch13_ex2.py
+++ week_4/ch13_ex2.py
@@ -19,12 +19,47 @@ def scheduler(availability):
     :param availability: dict, name: list of available days
     :return: dict, day: list of available names
     """
+    # initialize dict
+    schedule = {}   #  day: list of names
+    # iterate over the availability dict
+    for name in availability:    # availability {name: list of days}
+        days_list = availability[name]
+        # check each day; write down the day
+        # add available name to the list for that day
+        for day in days_list:
+            if day not in schedule:
+                schedule[day] = []    # if day was not previously added to the dict during the iteration history
+            schedule[day].append(name)
 
+    return schedule
+
+schedule = scheduler(availability)   # day: name list
 
 
 # find the day that most friends can attend
 
+# routine to find the "max"
+max_n_ppl = 0
+day_of_max_ppl = ''
 
+for day in schedule:
+    if len(schedule[day]) > max_n_ppl:
+        max_n_ppl = len(schedule[day])  # update the max number
+        day_of_max_ppl = day  # update the day
+
+print(f"The day that most people can attend: {day_of_max_ppl}")
 
 # List who can and can not attend the session that day
+
+ppl_attend = schedule[day_of_max_ppl]
+
+ppl_not_attend = []
+for day in schedule:
+    namelist = schedule[day]
+    for name in namelist:
+        if name not in ppl_attend:
+            ppl_not_attend.append(name)
+
+print(f"people who attend {ppl_attend}")
+print(f"people who do not attend {ppl_not_attend}")
 
